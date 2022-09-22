3f8b1ff662dd0fb907c23fa8121b97785057d602
petar-georgiev <georgiev.peter1@gmail.com>
2022-09-22 19:27:57 +0300

    Adding Jackson configuration for BeerDto.

--- src/main/java/com/example/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/example/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package com.example.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,21 +20,32 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto  {
+
     @Null
     private UUID id;
+
     @Null
     private Integer version;
+
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:MM:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
+
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:MM:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
+
     @NotBlank
     private String beerName;
+
     @NotNull
     private BeerStyleEnum beerStyle;
+
     @Positive
     @NotNull
     private Long upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
