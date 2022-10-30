3d1775cf6e7fd7ac3ce6c38c7d0b70ea2e7e6406
petar-georgiev <georgiev.peter1@gmail.com>
2022-10-30 23:20:13 +0200

    correcting Hibernate error with MySQL

--- src/main/java/com/example/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/com/example/msscbeerservice/MsscBeerServiceApplication.java
@@ -3,9 +3,10 @@ package com.example.msscbeerservice;
 import com.example.msscbeerservice.services.inventory.BeerInventoryServiceRestTemplateImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
